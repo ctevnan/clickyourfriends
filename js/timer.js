bb74243e64a9b86f1764a147594a8793ab4af128
ctevnan <carolyntevnan@yahoo.com>
2016-02-22 16:00:10 -0500

    trying out new strategy for game logic

--- js/timer.js
+++ js/timer.js
@@ -1,48 +1,7 @@
-var secondsCount = 0;
-var timerInterval;
-var toggleBtn = document.getElementById("toggleBtn");
+$(document).ready(function() {
+  var trackGameScore = 0;
+  
+  $("td img").addClass("img-responsive");
 
-setTimeout(function() {
-  if(secondsCount === 0){
-    startInterval(timerInterval);
-    toggleBtn.parentNode.removeChild("toggleBtn");
-  alert("Click the Shamoons as fast as you can!")
-  }
-}, 20000);
-
-function addSeconds() {
-  secondsCount++;
+  $(".img-responsive").attr("disabled", true);
 }
-//toggle btn is being clicked. get the data state attribute
-//if its equal to start then change the inner html to be stop
-//and change that attribute data-state to stop.
-//or else, do the opposite.
-function toggleTime() {
-  if(this.getAttribute("data-state") === "start") {
-    this.innerHTML = "Stop";
-    this.setAttribute("data-state", "stop");
-    secondsCount = 0;
-    timerInterval = setInterval(addSeconds, 1000);
-  } else {
-    this.innerHTML = "Start";
-    this.setAttribute("data-state", "start");
-    clearInterval(timerInterval);
-    alert("That took you " + secondsCount + " seconds");
-  }  
-}
-
-function clickCounter() {
-    if(typeof(Storage) !== "undefined") {
-        if (sessionStorage.clickcount) {
-            sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
-        } else {
-            sessionStorage.clickcount = 1;
-        }
-        document.getElementById("result").innerHTML = "You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
-    } else {
-        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
-    }
-}
-
-toggleBtn.addEventListener("click", toggleTime);
-clickCounterBtn.addEventListener("click", clickcount);
