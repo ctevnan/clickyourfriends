a09d8a1e8373f6d77a7552baa6796a145fe56a39
ctevnan <carolyntevnan@yahoo.com>
2016-02-22 16:19:35 -0500

    adding score modal

--- js/timer.js
+++ js/timer.js
@@ -4,4 +4,18 @@ $(document).ready(function() {
   $("td img").addClass("img-responsive");
 
   $(".img-responsive").attr("disabled", true);
+
+  function countInc() {
+    trackGameScore+=1;
+
+    $(".score").text(trackGameScore);
+  }
 }
+
+  function countDescd() {
+    setTimeout(function count() {
+      $("#scoreModal .modal-body").empty();
+
+      $("#scoreModal .modal-body").append("<p>The game is over. You clicked" + trackGameScore + " Shamoons.</p>");
+    }
+  }
